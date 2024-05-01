b91c07d5de9743cc1b88883b0f1456c86cb8fa5f
boon2187 <fumiya.mai.ito@gmail.com>
2024-05-01 22:11:19 +0900

    apiを呼び出しているが、成功していない。

---
+++ features/blog/components/MarkdownImage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import matter from "gray-matter";
+import Image from "next/image";
+
+const MarkdownImage = ({ markdown, width, height }) => {
+  const { data } = matter(markdown);
+  const imageUrl = data.image;
+  // console.log(width, height);
+
+  return (
+    <div>
+      {imageUrl && (
+        <Image
+          src={imageUrl}
+          alt="Blog Eyecatch"
+          width={width}
+          height={height}
+        />
+      )}
+    </div>
+  );
+};
+
+export default MarkdownImage;

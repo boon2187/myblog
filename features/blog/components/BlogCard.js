4109b92a2c6e426056f19bbd0682c9d6511d4577
boon2187 <fumiya.mai.ito@gmail.com>
2024-05-02 11:53:15 +0900

    一旦、マージンなどのCSSをなしに。

--- features/blog/components/BlogCard.js
+++ features/blog/components/BlogCard.js
@@ -4,38 +4,36 @@ import MarkdownImage from "./MarkdownImage";
 
 export default function BlogCard({ posts }) {
   return (
-    <div className="mt-10 space-y-16 border-t border-gray-300 pt-10 sm:mt-16 sm:pt-16">
+    <div className="mt-10 space-y-16 mx-auto border-t border-gray-400 pt-10 sm:mt-16 sm:pt-16">
       {posts.map((post) => (
-        <article
-          key={post.slug}
-          className="flex max-w-xl flex-col items-start justify-between"
-        >
-          <div>
+        <article key={post.slug} className="flex max-w-xl flex-row items-start">
+          <div className="">
             <MarkdownImage
+              className="bg-white"
               imageUrl={post.frontmatter.image}
               width={1024}
               height={1024}
             />
           </div>
-          <div className="group relative">
+          <div className="group">
             {/* 日付の表示 */}
-            <div className="flex items-center gap-x-4 text-xs">
+            <div className="text-xs">
               <div className="text-gray-500">
                 {new Date(post.frontmatter.date).toLocaleDateString()}
               </div>
             </div>
-            <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-100 group-hover:text-gray-400">
+            <h3 className="text-lg font-semibold leading-6 text-gray-100 group-hover:text-gray-400">
               {/* 記事タイトルへのリンク */}
               <Link
                 href={`/blog/${post.slug}`}
-                className="mt-3 text-lg font-semibold leading-6 text-gray-100 group-hover:text-gray-400"
+                className="text-lg font-semibold leading-6 text-gray-100 group-hover:text-gray-400"
               >
                 {post.frontmatter.title}
               </Link>
             </h3>
             {/* 記事の説明文（description）を表示 */}
             <p
-              className="mt-5 line-clamp-3 text-sm leading-6 text-gray-400"
+              className="line-clamp-3 text-sm leading-6 text-gray-400"
               dangerouslySetInnerHTML={{
                 __html: `${post.frontmatter.description}`,
               }}

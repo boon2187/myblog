47ca5e29011a4aa6008399b4fff2893f24508852
boon2187 <fumiya.mai.ito@gmail.com>
2024-05-10 14:26:58 +0900

    メニュー部分の高さを設定。メニュー項目にpaddingを設定。

--- features/common/components/HeaderMenu.jsx
+++ features/common/components/HeaderMenu.jsx
@@ -3,22 +3,22 @@ import React from "react";
 
 function HeaderMenu() {
   return (
-    <header className="flex justify-between	mx-auto max-w-2xl px-6 lg:px-8 text-gray-100">
+    <header className="flex justify-between	items-center h-16 mt-6 mx-auto max-w-2xl px-6 lg:px-8 text-gray-100">
       <div>
         <Link href={"/"}>ロゴ</Link>
       </div>
       <nav>
         <ul className="flex space-x-4">
-          <li>
+          <li className="px-4">
             <Link href={"/about"}>about</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link href={"/blog"}>blog</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link href={"/projects"}>projects</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link href={"/gear"}>gear</Link>
           </li>
         </ul>

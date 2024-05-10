7c4dbff4b91f6a2e16cfb9c5b5faa1e9e1666c67
boon2187 <fumiya.mai.ito@gmail.com>
2024-05-10 11:01:38 +0900

    とりあえず、メニュー項目を追加

--- features/common/components/HeaderMenu.jsx
+++ features/common/components/HeaderMenu.jsx
@@ -1,7 +1,32 @@
+import Link from "next/link";
 import React from "react";
 
 function HeaderMenu() {
-  return <div className="text-white">HeaderMenu</div>;
+  return (
+    <div className="text-white">
+      <header>
+        <div>
+          <Link href={"/"}>ロゴ</Link>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <Link href={"/about"}>about</Link>
+            </li>
+            <li>
+              <Link href={"/blog"}>blog</Link>
+            </li>
+            <li>
+              <Link href={"/projects"}>projects</Link>
+            </li>
+            <li>
+              <Link href={"/gear"}>gear</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
 }
 
 export default HeaderMenu;

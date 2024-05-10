142ff7aee286cb2f1cd83a2f96893bde3a08fadc
boon2187 <fumiya.mai.ito@gmail.com>
2024-05-10 13:04:17 +0900

    不要なdivタグを削除して、メニューの位置を調整

--- features/common/components/HeaderMenu.jsx
+++ features/common/components/HeaderMenu.jsx
@@ -3,29 +3,27 @@ import React from "react";
 
 function HeaderMenu() {
   return (
-    <div className="text-white">
-      <header>
-        <div>
-          <Link href={"/"}>ロゴ</Link>
-        </div>
-        <nav>
-          <ul>
-            <li>
-              <Link href={"/about"}>about</Link>
-            </li>
-            <li>
-              <Link href={"/blog"}>blog</Link>
-            </li>
-            <li>
-              <Link href={"/projects"}>projects</Link>
-            </li>
-            <li>
-              <Link href={"/gear"}>gear</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </div>
+    <header className="flex justify-between	mx-auto max-w-2xl px-6 lg:px-8 text-white">
+      <div>
+        <Link href={"/"}>ロゴ</Link>
+      </div>
+      <nav>
+        <ul className="flex space-x-4">
+          <li>
+            <Link href={"/about"}>about</Link>
+          </li>
+          <li>
+            <Link href={"/blog"}>blog</Link>
+          </li>
+          <li>
+            <Link href={"/projects"}>projects</Link>
+          </li>
+          <li>
+            <Link href={"/gear"}>gear</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 

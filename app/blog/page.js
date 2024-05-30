e46742fcb0efeb15872adf3ae30bd0b7243df616
boon2187 <fumiya.mai.ito@gmail.com>
2024-05-30 22:40:13 +0900

    すべての投稿データを受け取る処理を関数に切り出した。

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,41 +1,40 @@
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
-
 import BlogCard from "../../features/blog/components/BlogCard";
+import { getAllPosts } from "../../features/common/lib";
 
 export const metadata = {
   title: "My Blog",
 };
 
 export default async function Blogs() {
-  // contentディレクトリ内のマークダウンファイル一覧を取得
-  // postsDirectoryは './conent/'
-  const postsDirectory = path.join(process.cwd(), "content");
-  // fs.readdirSync()でディレクトリ内のファイル名を取得
-  const fileNames = fs.readdirSync(postsDirectory);
+  const posts = await getAllPosts();
+  // console.log(allPosts);
+  // // contentディレクトリ内のマークダウンファイル一覧を取得
+  // // postsDirectoryは './conent/'
+  // const postsDirectory = path.join(process.cwd(), "content");
+  // // fs.readdirSync()でディレクトリ内のファイル名を取得
+  // const fileNames = fs.readdirSync(postsDirectory);
 
-  // 各ファイルの中身を取得
-  const posts = await Promise.all(
-    // 各ファイル情報を取得
-    fileNames.map(async (fileName) => {
-      // ファイルのpathを取得
-      const filePath = path.join(postsDirectory, fileName);
-      // ファイルの中身を取得
-      const fileContents = fs.readFileSync(filePath, "utf8");
-      // ファイルのメタデータを取得
-      const { data } = matter(fileContents);
+  // // 各ファイルの中身を取得
+  // const posts = await Promise.all(
+  //   // 各ファイル情報を取得
+  //   fileNames.map(async (fileName) => {
+  //     // ファイルのpathを取得
+  //     const filePath = path.join(postsDirectory, fileName);
+  //     // ファイルの中身を取得
+  //     const fileContents = fs.readFileSync(filePath, "utf8");
+  //     // ファイルのメタデータを取得
+  //     const { data } = matter(fileContents);
 
-      // slugとfrontmatterを返す
-      return {
-        slug: fileName.replace(/\.md$/, ""),
-        frontmatter: data,
-      };
-    })
-  ).then((posts) =>
-    // 最新日付順に並び替え
-    posts.sort((a, b) => (a.frontmatter.date < b.frontmatter.date ? 1 : -1))
-  );
+  //     // slugとfrontmatterを返す
+  //     return {
+  //       slug: fileName.replace(/\.md$/, ""),
+  //       frontmatter: data,
+  //     };
+  //   })
+  // ).then((posts) =>
+  //   // 最新日付順に並び替え
+  //   posts.sort((a, b) => (a.frontmatter.date < b.frontmatter.date ? 1 : -1))
+  // );
 
   return (
     <div className="bg-black py-24 sm:py-32">

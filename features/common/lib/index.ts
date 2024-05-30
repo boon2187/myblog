1c9cb6fc90c22c8e05297ab6477b60dcbb20c46e
boon2187 <fumiya.mai.ito@gmail.com>
2024-05-30 22:24:58 +0900

    すべてのMDファイルからすべての投稿データをpostsとして返す関数を作成

---
+++ features/common/lib/index.ts
@@ -0,0 +1,35 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+export const getAllPosts = async () => {
+  // contentディレクトリ内のマークダウンファイル一覧を取得
+  // postsDirectoryは './conent/'
+  const postsDirectory = path.join(process.cwd(), "content");
+  // fs.readdirSync()でディレクトリ内のファイル名を取得
+  const fileNames = fs.readdirSync(postsDirectory);
+
+  // 各ファイルの中身を取得
+  const posts = await Promise.all(
+    // 各ファイル情報を取得
+    fileNames.map(async (fileName) => {
+      // ファイルのpathを取得
+      const filePath = path.join(postsDirectory, fileName);
+      // ファイルの中身を取得
+      const fileContents = fs.readFileSync(filePath, "utf8");
+      // ファイルのメタデータを取得
+      const { data } = matter(fileContents);
+
+      // slugとfrontmatterを返す
+      return {
+        slug: fileName.replace(/\.md$/, ""),
+        frontmatter: data,
+      };
+    })
+  ).then((posts) =>
+    // 最新日付順に並び替え
+    posts.sort((a, b) => (a.frontmatter.date < b.frontmatter.date ? 1 : -1))
+  );
+
+  return posts;
+};

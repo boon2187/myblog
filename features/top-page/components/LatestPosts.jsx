a94d7428f788daf84e6928370e1278640008492b
boon2187 <fumiya.mai.ito@gmail.com>
2024-06-01 19:09:09 +0900

    すべてのPostを再取得するのでなく、親コンポーネントから受け取って、そのうち最新の３つをlatestPostsにした。

--- features/top-page/components/LatestPosts.jsx
+++ features/top-page/components/LatestPosts.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { getAllPosts } from "../../common/lib";
 import BlogCard from "../../blog/components/BlogCard";
 
-async function LatestPosts() {
-  const allposts = await getAllPosts();
-  const latestPosts = allposts.slice(0, 3);
-
+async function LatestPosts({ posts }) {
+  const latestPosts = posts.slice(0, 3);
+  console.log(latestPosts[0]);
   return (
     <div className="text-white w-1/2">
+      <p>{new Date(latestPosts[0].frontmatter.date).toLocaleDateString()}</p>
+      <p>{latestPosts[0].frontmatter.title}</p>
       <BlogCard posts={latestPosts} />
     </div>
   );

b11298e999ba231449d1a4dc4e2172ebd37ad93e
boon2187 <fumiya.mai.ito@gmail.com>
2024-06-01 19:43:42 +0900

    タイトルや説明を加えて、スマホに対応

--- features/top-page/components/LatestPosts.jsx
+++ features/top-page/components/LatestPosts.jsx
@@ -5,15 +5,31 @@ async function LatestPosts({ posts }) {
   const latestPosts = posts.slice(0, 3);
 
   return (
-    <div className="text-white w-1/2">
-      {latestPosts.map((post) => (
-        <div key={post.slug}>
-          <Link href={`/blog/${post.slug}`}>
-            <p>{new Date(post.frontmatter.date).toLocaleDateString()}</p>
-            <p>{post.frontmatter.title}</p>
-          </Link>
-        </div>
-      ))}
+    <div>
+      <h2 className="text-2xl font-semibold text-gray-100">Latest Posts</h2>
+      <p className="text-neutral-400 my-3">
+        I occasionally write about programming, especially Flutter, React,
+        Next.js, and more. Check me out to stay up to date.
+      </p>
+      <ul>
+        {latestPosts.map((post) => (
+          <li
+            key={post.slug}
+            className="flex flex-col py-3 sm:flex-row sm:py-3 sm:gap-x-14"
+          >
+            <Link href={`/blog/${post.slug}`}>
+              <h2 className="text-neutral-300">
+                {new Date(post.frontmatter.date).toLocaleDateString()}
+              </h2>
+            </Link>
+            <Link href={`/blog/${post.slug}`}>
+              <p className="font-semibold text-gray-100">
+                {post.frontmatter.title}
+              </p>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }

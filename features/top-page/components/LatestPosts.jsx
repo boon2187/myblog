b30465d359038c4b792973f57d950d5bdf037f8d
boon2187 <fumiya.mai.ito@gmail.com>
2024-06-01 19:13:45 +0900

    latestPostsをマップ関数で日付とタイトルを表示して、リンクもつけた。

--- features/top-page/components/LatestPosts.jsx
+++ features/top-page/components/LatestPosts.jsx
@@ -1,15 +1,19 @@
 import React from "react";
-import { getAllPosts } from "../../common/lib";
-import BlogCard from "../../blog/components/BlogCard";
+import Link from "next/link";
 
 async function LatestPosts({ posts }) {
   const latestPosts = posts.slice(0, 3);
-  console.log(latestPosts[0]);
+
   return (
     <div className="text-white w-1/2">
-      <p>{new Date(latestPosts[0].frontmatter.date).toLocaleDateString()}</p>
-      <p>{latestPosts[0].frontmatter.title}</p>
-      <BlogCard posts={latestPosts} />
+      {latestPosts.map((post) => (
+        <div key={post.slug}>
+          <Link href={`/blog/${post.slug}`}>
+            <p>{new Date(post.frontmatter.date).toLocaleDateString()}</p>
+            <p>{post.frontmatter.title}</p>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 }

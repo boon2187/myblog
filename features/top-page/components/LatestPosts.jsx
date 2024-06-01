c4409e2119cedd1ebf8963e39543e2cc36f6c9d2
boon2187 <fumiya.mai.ito@gmail.com>
2024-06-01 20:11:11 +0900

    スマホのときはアイキャッチを表示。あとは、表示の微調整

--- features/top-page/components/LatestPosts.jsx
+++ features/top-page/components/LatestPosts.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import MarkdownImage from "../../blog/components/MarkdownImage";
 
 async function LatestPosts({ posts }) {
   const latestPosts = posts.slice(0, 3);
@@ -11,22 +12,32 @@ async function LatestPosts({ posts }) {
         I occasionally write about programming, especially Flutter, React,
         Next.js, and more. Check me out to stay up to date.
       </p>
-      <ul>
+      <ul className="">
         {latestPosts.map((post) => (
-          <li
-            key={post.slug}
-            className="flex flex-col py-3 sm:flex-row sm:gap-x-14"
-          >
-            <Link href={`/blog/${post.slug}`}>
-              <h2 className="text-neutral-300">
-                {new Date(post.frontmatter.date).toLocaleDateString()}
-              </h2>
-            </Link>
-            <Link href={`/blog/${post.slug}`}>
-              <p className="font-semibold text-gray-100">
-                {post.frontmatter.title}
-              </p>
-            </Link>
+          <li key={post.slug} className="flex items-center">
+            <div className="flex-1">
+              <div className="flex flex-col py-3 sm:flex-row ">
+                <Link href={`/blog/${post.slug}`} className="sm:w-1/5">
+                  <h2 className="text-neutral-300">
+                    {new Date(post.frontmatter.date).toLocaleDateString()}
+                  </h2>
+                </Link>
+                <Link href={`/blog/${post.slug}`}>
+                  <p className="font-semibold text-gray-100">
+                    {post.frontmatter.title}
+                  </p>
+                </Link>
+              </div>
+            </div>
+            <div className="py-1 sm:hidden">
+              <Link href={`/blog/${post.slug}`}>
+                <MarkdownImage
+                  imageUrl={post.frontmatter.image}
+                  width={100}
+                  height={100}
+                />
+              </Link>
+            </div>
           </li>
         ))}
       </ul>

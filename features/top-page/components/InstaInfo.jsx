49849cefe019b7843042e5080c7569c75a5329e2
boon2187 <fumiya.mai.ito@gmail.com>
2024-06-11 11:28:29 +0900

    全体のサイズを調整するために、とりあえず、Twitterと同じ内容をInstaコンポーネントにも入れた

--- features/top-page/components/InstaInfo.jsx
+++ features/top-page/components/InstaInfo.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 function InstaInfo() {
   return (
-    <div className='text-white'>InstaInfo</div>
-  )
+    <div className="flex">
+      <div>
+        <Image
+          src={"/images/twitter.png"}
+          width={100}
+          height={100}
+          alt="profile"
+        />
+      </div>
+      <div>
+        <p className="text-white">Please follow me.</p>
+      </div>
+    </div>
+  );
 }
 
-export default InstaInfo
+export default InstaInfo;

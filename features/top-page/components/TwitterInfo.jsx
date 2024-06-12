8eb126c455a76e96a8bb451bd2ac77fc6965434f
boon2187 <fumiya.mai.ito@gmail.com>
2024-06-12 19:06:38 +0900

    見た目を整える。スマホ対応はこれから

--- features/top-page/components/TwitterInfo.jsx
+++ features/top-page/components/TwitterInfo.jsx
@@ -1,19 +1,28 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 function TwitterInfo() {
   return (
-    <div className="flex w-1/2  bg-neutral-900">
-      <div>
-        <Image
-          src={"/images/twitter.png"}
-          width={100}
-          height={100}
-          alt="profile"
-        />
+    <div className="flex w-full sm:w-1/2  bg-neutral-900 py-3">
+      <div className="py-1 px-3 w-1/3">
+        <Link href="https://twitter.com/boon2187">
+          <Image
+            src={"/images/twitter.png"}
+            width={100}
+            height={100}
+            alt="profile"
+            className="rounded-xl"
+          />
+        </Link>
       </div>
-      <div>
-        <p className="text-white">Please follow me.</p>
+      <div className="px-1 my-auto w-2/3">
+        <Link href="https://twitter.com/boon2187">
+          <h2 className="text-lg font-semibold text-gray-100">My X account</h2>
+        </Link>
+        <p className="text-neutral-400 mt-1">
+          I usually posts sssssss ssss sssss sssssss my ssss ssss ssssssssssss
+        </p>
       </div>
     </div>
   );

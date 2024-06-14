34feb8b6c0f82e890332f641f3ba91ccf8df44e8
boon2187 <fumiya.mai.ito@gmail.com>
2024-06-14 16:33:04 +0900

    Twitterのコンポーネントをそのままコピー

--- features/top-page/components/InstaInfo.jsx
+++ features/top-page/components/InstaInfo.jsx
@@ -5,23 +5,23 @@ import React from "react";
 function InstaInfo() {
   return (
     <div className="flex rounded bg-neutral-900 py-3">
-      <div className="py-1 px-3 w-1/3">
+      <div className="py-1 px-3">
         <Link href="https://twitter.com/boon2187">
           <Image
             src={"/images/twitter.png"}
             width={100}
             height={100}
             alt="profile"
-            className="rounded-xl"
+            className="rounded-xl min-w-20"
           />
         </Link>
       </div>
-      <div className="px-1 my-auto w-2/3">
+      <div className="px-1">
         <Link href="https://twitter.com/boon2187">
           <h2 className="text-lg font-semibold text-gray-100">My X account</h2>
         </Link>
         <p className="text-neutral-400 mt-1">
-          I usually posts sssssss ssss sssss sssssss my ssss ssss ssssssssssss
+          I post about my personal development, work, family, and more.
         </p>
       </div>
     </div>

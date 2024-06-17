ab851b401a54c87aab7a4f181b7df0b2acea2ea1
boon2187 <fumiya.mai.ito@gmail.com>
2024-06-18 00:03:47 +0900

    とりあえず、Aboutページの構成・マークアップだけやってみた

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const About = () => {
-  return <div className="text-white">About</div>;
+  return (
+    <>
+      <h1 className="text-white">About</h1>
+      <div>
+        <div className="text-white">プロフィールのイラスト</div>
+        <div>
+          <div className="text-white">槍ヶ岳の写真</div>
+          <div className="text-white">オンデーズでの</div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default About;

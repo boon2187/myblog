c3baa8cf219d36ab0f3dd4221d78aa710eb2de4d
boon2187 <fumiya.mai.ito@gmail.com>
2024-06-20 13:01:24 +0900

    写真部分の構成を作る

--- app/about/page.js
+++ app/about/page.js
@@ -2,16 +2,24 @@ import React from "react";
 
 const About = () => {
   return (
-    <>
-      <h1 className="text-white">About</h1>
-      <div>
-        <div className="text-white">プロフィールのイラスト</div>
-        <div>
-          <div className="text-white">槍ヶ岳の写真</div>
-          <div className="text-white">オンデーズでの</div>
+    <div className="bg-black py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl">
+          <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+            About
+          </h2>
+          <div className="mt-10 space-y-16 mx-auto border-t border-gray-400 pt-10 sm:mt-16 sm:pt-16">
+            <div className="flex flex-col sm:flex-row">
+              <div className="text-white">プロフィールのイラスト</div>
+              <div>
+                <div className="text-white">槍ヶ岳の写真</div>
+                <div className="text-white">オンデーズでの</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 

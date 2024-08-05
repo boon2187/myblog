4894eead460d1dd843dd44a5f5a03fc421554c47
boon2187 <fumiya.mai.ito@gmail.com>
2024-08-05 13:48:01 +0900

    各カード部分の見た目を変更

--- app/projects/components/project-list.jsx
+++ app/projects/components/project-list.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const ProjectList = async () => {
@@ -9,7 +10,12 @@ const ProjectList = async () => {
     <ul className="text-white">
       {repos.map((repo) => (
         <li key={repo.id} className="bg-gray-800 p-4 rounded-lg mb-4">
-          <h3 className="text-xl font-bold text-white">{repo.name}</h3>
+          <div className="flex justify-between items-center mb-4">
+            <Link href={repo.html_url}>
+              <h3 className="text-xl font-bold text-white">{repo.name}</h3>
+            </Link>
+            <div>🌟{repo.stargazers_count}</div>
+          </div>
           <p className="text-gray-400">{repo.description}</p>
         </li>
       ))}

e33e2508b874413c184ac720803be69a46b9b5b6
boon2187 <fumiya.mai.ito@gmail.com>
2024-08-05 13:34:39 +0900

    他のページと書式を揃えた

--- app/projects/page.js
+++ app/projects/page.js
@@ -3,8 +3,17 @@ import ProjectList from "./components/project-list";
 
 const Projects = () => {
   return (
-    <div>
-      <ProjectList />
+    <div className="bg-black py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl">
+          <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+            My Projects
+          </h2>
+          <div className="mt-10 space-y-16 mx-auto border-t border-gray-400 pt-10 sm:mt-16 sm:pt-16">
+            <ProjectList />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

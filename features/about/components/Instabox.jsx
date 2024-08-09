f72f8cbf5220cebd1e7f72e25aa23960ac04da79
boon2187 <fumiya.mai.ito@gmail.com>
2024-08-09 11:19:35 +0900

    とりあえず、トップページと同じものを追加

---
+++ features/about/components/Instabox.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function Instabox() {
+  return (
+    <div className="flex rounded bg-neutral-900 py-3">
+      <div className="py-1 px-3">
+        <Link href="https://www.instagram.com/boon_boon/">
+          <Image
+            src={"/images/instgram-logo.png"}
+            width={100}
+            height={100}
+            alt="profile"
+            className="rounded-xl min-w-20"
+          />
+        </Link>
+      </div>
+      <div className="px-1">
+        <Link href="https://www.instagram.com/boon_boon/">
+          <h2 className="text-lg font-semibold text-gray-100">My Instagram</h2>
+        </Link>
+        <p className="text-neutral-400 mt-1">
+          I usually post pictures of the mountains and skies of my hometown,
+          Matsumoto.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Instabox;

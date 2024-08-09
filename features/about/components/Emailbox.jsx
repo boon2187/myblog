6be8dc061968dc029e2f2284ca8db14dc3eae743
boon2187 <fumiya.mai.ito@gmail.com>
2024-08-09 12:25:07 +0900

    アイコン、キャプション、リンク先を修正

--- features/about/components/Emailbox.jsx
+++ features/about/components/Emailbox.jsx
@@ -1,4 +1,5 @@
-import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram, faXTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import Link from "next/link";
@@ -6,13 +7,13 @@ import React from "react";
 
 function Emailbox() {
   return (
-    <Link href="https://www.instagram.com/boon_boon/">
+    <Link href="mailto:boonboon@flutter-newbie.com">
       <div className="flex rounded bg-neutral-900 py-3 h-20 items-center">
         <div className="py-1 px-3">
-          <FontAwesomeIcon icon={faInstagram} size="2x" />
+          <FontAwesomeIcon icon={faEnvelope} size="2x" />{" "}
         </div>
         <div className="px-1">
-          <h2 className="font-semibold text-gray-100">My Instagram</h2>
+          <h2 className="font-semibold text-gray-100">My Mail</h2>
         </div>
       </div>
     </Link>
--- features/about/components/Twitterbox.jsx
+++ features/about/components/Twitterbox.jsx
@@ -1,4 +1,4 @@
-import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faXTwitter } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import Link from "next/link";
@@ -6,13 +6,13 @@ import React from "react";
 
 function Twitterbox() {
   return (
-    <Link href="https://www.instagram.com/boon_boon/">
+    <Link href="https://twitter.com/boon2187">
       <div className="flex rounded bg-neutral-900 py-3 h-20 items-center">
         <div className="py-1 px-3">
-          <FontAwesomeIcon icon={faInstagram} size="2x" />
+          <FontAwesomeIcon icon={faXTwitter} size="2x" />
         </div>
         <div className="px-1">
-          <h2 className="font-semibold text-gray-100">My Instagram</h2>
+          <h2 className="font-semibold text-gray-100">My X account</h2>
         </div>
       </div>
     </Link>

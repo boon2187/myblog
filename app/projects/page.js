3c544d7ee7fabacfea5c638b1ccb67056dd674a8
boon2187 <fumiya.mai.ito@gmail.com>
2024-08-09 13:12:51 +0900

    ProjectsListコンポーネントの移動に伴い、パスを変更

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { ErrorBoundary } from "react-error-boundary";
-import ProjectList from "./components/project-list";
+import ProjectList from "../../features/projects/conponents/project-list";
 
 export default function Projects() {
   return (

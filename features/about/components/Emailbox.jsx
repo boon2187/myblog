5df4a508db2ad3774c01dff0ba043ae83d769b1d
boon2187 <fumiya.mai.ito@gmail.com>
2024-08-09 12:16:17 +0900

    TiwtterとEmailのboxをInstagramのをコピペでとりあえず作成

---
+++ features/about/components/Emailbox.jsx
@@ -0,0 +1,22 @@
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import Link from "next/link";
+import React from "react";
+
+function Emailbox() {
+  return (
+    <Link href="https://www.instagram.com/boon_boon/">
+      <div className="flex rounded bg-neutral-900 py-3 h-20 items-center">
+        <div className="py-1 px-3">
+          <FontAwesomeIcon icon={faInstagram} size="2x" />
+        </div>
+        <div className="px-1">
+          <h2 className="font-semibold text-gray-100">My Instagram</h2>
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+export default Emailbox;

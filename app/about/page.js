cdf12001d926fd45d542e12b4bc3c1efa8fb374f
boon2187 <fumiya.mai.ito@gmail.com>
2024-08-09 12:57:02 +0900

    動画の高さを微調整

--- app/about/page.js
+++ app/about/page.js
@@ -102,7 +102,7 @@ const About = () => {
                   title="YouTube video player"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                   referrerPolicy="strict-origin-when-cross-origin"
-                  style={{ border: 0, width: "100%", height: "400px" }}
+                  style={{ border: 0, width: "100%", height: "300px" }}
                   allowFullScreen
                 ></iframe>
               </div>

277fd670287f75a9933bb5cebfbb1d87029bd745
boon2187 <fumiya.mai.ito@gmail.com>
2024-08-09 11:31:17 +0900

    Instagramのアイコンを画像ではなく、FontAwesomeアイコンに

--- features/about/components/Instabox.jsx
+++ features/about/components/Instabox.jsx
@@ -1,3 +1,6 @@
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -7,23 +10,13 @@ function Instabox() {
     <div className="flex rounded bg-neutral-900 py-3">
       <div className="py-1 px-3">
         <Link href="https://www.instagram.com/boon_boon/">
-          <Image
-            src={"/images/instgram-logo.png"}
-            width={100}
-            height={100}
-            alt="profile"
-            className="rounded-xl min-w-20"
-          />
+          <FontAwesomeIcon icon={faInstagram} size="2x" />
         </Link>
       </div>
       <div className="px-1">
         <Link href="https://www.instagram.com/boon_boon/">
           <h2 className="text-lg font-semibold text-gray-100">My Instagram</h2>
         </Link>
-        <p className="text-neutral-400 mt-1">
-          I usually post pictures of the mountains and skies of my hometown,
-          Matsumoto.
-        </p>
       </div>
     </div>
   );

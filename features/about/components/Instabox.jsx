cd404e6cc837287df9a2d2bf54ee67eeedba73f8
boon2187 <fumiya.mai.ito@gmail.com>
2024-08-09 12:14:33 +0900

    不要なimportを削除

--- features/about/components/Instabox.jsx
+++ features/about/components/Instabox.jsx
@@ -1,7 +1,6 @@
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 

7cfc3af126e03b2104b4d2f604218227f982d92f
boon2187 <fumiya.mai.ito@gmail.com>
2024-08-10 22:11:24 +0900

    見た目を整えて、「建設中のページ」でとりあえず、Gearページを完成とする

--- app/gear/page.js
+++ app/gear/page.js
@@ -1,4 +1,6 @@
 import React from "react";
+import profile from "../../public/images/profile_image.png";
+import Image from "next/image";
 
 export const metadata = {
   title: "My Gear",
@@ -13,10 +15,25 @@ export default function Gear() {
             My Gear
           </h2>
           <div className="mt-10 space-y-16 mx-auto border-t border-gray-400 pt-10 sm:mt-16 sm:pt-16">
-            <div className="text-neutral-400 mb-8">
-              This is the list of my GitHub repos.
+            <div className="container flex">
+              <div className="w-1/4">
+                <Image
+                  src={profile}
+                  width={100}
+                  height={100}
+                  alt="profile"
+                  className="w-full"
+                />
+              </div>
+              <div className="my-auto">
+                <h2 className="text-neutral-400 text-2xl font-semibold">
+                  Sorry....
+                </h2>
+                <p className="text-neutral-400 m-3">
+                  This page is under construction...
+                </p>
+              </div>
             </div>
-            <div className="max-w-xl mx-auto"></div>
           </div>
         </div>
       </div>

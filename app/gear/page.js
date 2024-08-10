2d97c6c179159336589a6151c89ece4f9e3d65d4
boon2187 <fumiya.mai.ito@gmail.com>
2024-08-10 21:39:19 +0900

    Gearのページを新規に作成

---
+++ app/gear/page.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const metadata = {
+  title: "My Gear",
+};
+
+export default function Gear() {
+  return (
+    <div className="bg-black py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl">
+          <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+            My Gear
+          </h2>
+          <div className="mt-10 space-y-16 mx-auto border-t border-gray-400 pt-10 sm:mt-16 sm:pt-16">
+            <div className="text-neutral-400 mb-8">
+              This is the list of my GitHub repos.
+            </div>
+            <div className="max-w-xl mx-auto"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}

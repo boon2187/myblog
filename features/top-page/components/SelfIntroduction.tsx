c6a1c687e9f3aa9caac8e79995e1404984a7754d
boon2187 <fumiya.mai.ito@gmail.com>
2026-04-26 20:56:25 +0900

    トップページのディスクリプションを修正

--- features/top-page/components/SelfIntroduction.tsx
+++ features/top-page/components/SelfIntroduction.tsx
@@ -16,7 +16,7 @@ const SelfIntroduction = () => {
         <h2 className="text-3xl l font-bold">Fumitsugu Ito</h2>
         <p className="text-neutral-400 pt-3">
           Hi there! I am a software engineer who build web sites and web apps
-          using React, Next.js and WordPress(PHP). And also I build Smartphone
+          using React, Next.js and Laravel. And also I build Smartphone
           Apps with Flutter for my personal projects. One more thing.... I work
           for challenged person&apos;s support for employment in Matsumoto, my
           hometown. I advised them about IT, software engineering and
